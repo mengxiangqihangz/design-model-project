eac681ca927e5888919eea87bcaa3ceafcd3e46f
mengxiangqihangz <zhiang082436>
2018-07-15 11:17:15 +0800

    补全

--- design-model-1.1/src/net/dp/command/simpleremote/RemoteControlTest.java
+++ design-model-1.1/src/net/dp/command/simpleremote/RemoteControlTest.java
@@ -1,11 +1,19 @@
 package net.dp.command.simpleremote;
 
+
+/**
+ * 其核心在于控制类 SimpleRemoteControl
+ * 对于它，注入不同的对象，执行buttonWasPressed 的得到的结果不同，这个早就写过，对于spring，注入不同的dao层，跑出不同的结果。
+ * @author liangpro
+ *
+ */
 public class RemoteControlTest {
 	public static void main(String[] args) {
 		SimpleRemoteControl remote = new SimpleRemoteControl();
 		Light light = new Light();
-		GarageDoor garageDoor = new GarageDoor();
 		LightOnCommand lightOn = new LightOnCommand(light);
+		
+		GarageDoor garageDoor = new GarageDoor();
 		GarageDoorOpenCommand garageOpen = new GarageDoorOpenCommand(garageDoor);
 
 		remote.setCommand(lightOn);
